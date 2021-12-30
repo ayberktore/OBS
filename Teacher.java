b86d95cc1a8a798f1d15cd795a86add8f75f2edd
ayberktore <69429236+ayberktore@users.noreply.github.com>
2021-12-30 19:43:05 +0300

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,18 @@
+package Deneme;
+
+public class Teacher {
+
+    String name;
+    String mnpo;
+    String branch;
+
+    Teacher(String name, String mnpo, String branch)
+    {
+        this.name=name;
+        this.mnpo=mnpo;
+        this.branch=branch;
+
+    }
+
+
+}
